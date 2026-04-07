7473cf6bb5f6ace7a3311a5c51709be60420d26e
Lucas <lucasmazutti10@gmail.com>
2026-04-07 19:49:58 -0300

    Primeiro commit

---
+++ ProjetoDaDisciplina.py
@@ -0,0 +1,39 @@
+while True:
+    primeiro_comando = str(input("Digite 'about' para saber mais ou 'quit' para sair: ")).lower()
+    if primeiro_comando == "about":
+        print("Gestor de projetos do Lucas Mazutti!")
+        primeiro_comando = str(input("Digite add para adicionar o número de projetos: ")).lower()
+    if primeiro_comando.startswith("add"):
+        numero_projetos = int(input("Numero de projetos que queira adicionar -> "))
+        if numero_projetos <= 0:
+            print("Valor incorreto, digite um número maior que zero.")
+        else:
+            for i in range(numero_projetos):
+                nome_projeto = input(f"Digite o nome do projeto {i+1}: ")
+                print(f"SUCESSO: Projeto '{nome_projeto}' adicionado.")
+            print("Projetos adicionados!")
+    elif primeiro_comando == "quit":
+        print("Você saiu do gestor de projetos, até logo!")
+        break
+    else:
+        print("Valor não reconhecido, digite 'about', 'add' ou 'quit'.")
+
+'''
+inicio - "about"
+fim - "projetos adicionados"
+
+1. usuario digita about e segue para uma mensagem de boas vindas
+ - após isso uma segunda mensagem pedindo add
+ - se digitar quit, mensagem de despedida.
+ - outra coisa, erro e voltar ao inicio
+2. ao digitar add vai para o numero de projetos que o usuario quer adicionar
+ - se digitar outra coisa apenas refaz a mesma situação.
+3. ao digitar o numero de projetos para adicioonar(não pode ser 0 nem menor que 0) 4 eetapa.
+ - se menor igual a zero, mensagem de erro e retrornando ao menu principal do para ele 
+ digitar o numero corretamente.
+4. Digitandoo o numeoro de projetos, o sistema pede para escrever o nome dos projetos.
+5. após isso, mensagem de sucesso, projets gravados.
+
+'''
+
+
